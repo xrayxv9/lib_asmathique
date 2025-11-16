e7d49bbe25f30041761bcb2996129f4f67722a60
xray <xrayxv.5@gmail.com>
2025-11-16 22:06:57 +0100

    all the commands

--- ft_read.s
+++ ft_read.s
@@ -1,5 +1,21 @@
 section .text
 
+extern __errno_location
 global ft_read
 
-ft_read
+ft_read:
+	mov eax, 0 ; syscall for read
+	syscall 
+	test rax, rax
+	js .exit ; je jump si mon flag de sign est actif donc si je suis sur un nombre negatif donc erreur
+	ret
+
+.exit:
+	mov rdi, rax
+	neg rdi
+
+	call __errno_location wrt ..plt
+	
+	mov [rax], rdi
+	mov rax, -1
+	ret
--- ft_write.s
+++ ft_write.s
@@ -6,15 +6,15 @@ global ft_write
 ft_write:
 	mov rax, 1 ; deplace 1 dans rax car il s'agit du numero de sysccall pour le write
 	syscall ; besoin de rien de plus car tout les arguments sont deja dans rdi rsi et rdx
-	cmp rax, 1
-	jle .exit
+	test rax, rax ; test bit par bit avec & et met a jour les flags
+	js .exit ; jump si mon rax est negatif
 	ret ; return la len si tout ce pass bien
 
 .exit:
 	mov rdi, rax ; stock le retour du syscall dans mon rdi pour ne pas le perdre
 	neg rdi ; passe en positif la valeur erno retourné
 
-	call __errno_location wrt ..plt ; call la table plt de mon errno ( en pointeur )
+	call __errno_location wrt ..plt ; je recupere l'emplacement de ma table errno dans ma memoire dans mon rax
 
 	mov [rax], rdi ; met le rdi dans la variable global erno
 	mov rax, -1 ; return -1
