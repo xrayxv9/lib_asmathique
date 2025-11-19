8d536753615d9ae99638d2532e7efce68b6d56ce
xray <morel.clement@proton.me>
2025-11-19 17:02:07 +0100

    doing the while for the + and  -, don't know how to make it positive or negative but alr

--- ft_atoi_base.s
+++ ft_atoi_base.s
@@ -1,11 +1,98 @@
 section .text
 
 global ft_atoi_base
+global ft_strlen
 
 ft_atoi_base:
-	jmp .parsing_base
+	push rdi
+	push rsi
+	mov rdi, rsi
+	call parsing_base
+	cmp rax, -1
+	je .error_main
+	pop rsi
+	pop rdi
+	mov rax, 0
+.loop_skip_white_space
+	mov al, byte [rsi]
+	test al, al
+	jz .ret_rax
+	cmp al, ' '
+	je .inc_loop
+	sub al, 9
+	cmp al, 4
+	jbe .inc_loop
+.loop_minus_plus
+	mov al, byte [rsi]
+	test al, al
+	jz .ret_rax
+	cmp al, '-'
+	je .inc_loop
+	cmp al, '+'
+	je .inc_loop
 
-.parsing_base:
+
 	
+.inc_loop
+	inc rsi
+	jmp .loop_skip_white_space
+
+parsing_base:
+	test rdi, rdi
+	jz .error
+	cmp byte [rdi], 0
+	je .error
+	cmp byte [rdi + 1], 0
+	je .error
+
+.first_loop:
+	mov al, byte [rdi]
+	test al, al
+	jz .end_parsing
+	cmp al, '+'
+	je .error
+	cmp al, '-'
+	je .error
+	lea rax, [rdi + 1]
+	inc rdi
+
+.second_loop:
+	mov cl, byte [rax]
+	test cl, cl
+	jz .first_loop
+	cmp cl, al
+	je .error
+	inc rax
+	jmp .second_loop
+
+; rdi : chaine de string
+; rsi : char
+char_in_base:
+	mov rax, 0
+.loop_char:
+	mov al, byte [rdi]
+	test al, al
+	jz .error
+	cmp al, rsi
+	je .ret_rax
+	inc rax
+	inc rdi
+	jmp .loop_char
+
 	
-	
+.ret_rax:
+	ret
+
+.finish_char:
+	mov rax, 
+.error:
+	mov rax, -1
+	ret
+
+.end_parsing:
+	mov rax, 0
+	ret
+
+.error_main:
+	mov rax, 0
+	ret
--- ft_strcmp.s
+++ ft_strcmp.s
@@ -3,14 +3,14 @@ section .text
 global ft_strcmp
 
 ft_strcmp:
-	mov cl, byte [rdi]
-	cmp cl, byte [rsi]
-	jne .exit
-	test cl, byte [rsi]
-	je .exit
-	inc rdi
+	mov cl, byte [rdi] ; je ne peux pas faire deux fois byte sur une ligne donc je met le byte dans le cl
+	cmp cl, byte [rsi] ; je fais la comparaison
+	jne .exit ; jump non equal
+	test cl, cl ; je check si c'est egale a \0
+	je .exit ; jump equal
+	inc rdi ; j'incremente mes chaines
 	inc rsi
-	jmp ft_strcmp
+	jmp ft_strcmp ; je retourne au debut
 
 .exit:
 	movzx eax, byte [rdi]
