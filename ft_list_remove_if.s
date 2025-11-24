a1b2960df0aabc1cc8ef277d30c5e6a8849666fe
xray <morel.clement@proton.me>
2025-11-24 13:46:32 +0100

    Libasm finished

--- ft_list_remove_if.s
+++ ft_list_remove_if.s
@@ -5,8 +5,8 @@ extern free
 
 ; rdi: t_list **
 ; rsi: void *data_ref
-; rcx: (*cmp)()
-; rdx: (*free_fct)(void *)
+; rdx: (*cmp)()
+; rcx: (*free_fct)(void *)
 ft_list_remove_if:
 	test rdi, rdi
 	jz .finish
@@ -18,20 +18,24 @@ ft_list_remove_if:
 	jz .finish
 	test rdx, rdx
 	jz .finish
+	
+	push r12
+	push r13
+	push r14
+	push r15
 
+	mov r12, rdi
+	mov r13, rsi
+	mov r14, rdx
+	mov r15, rcx
 
 .first_element:
-	mov rbx, [rdi]
-	push rdi
+	mov rbx, [r12]
+	test rbx, rbx
+	je .finish
 	mov rdi, [rbx + 8]
-	push rbx
-	push rcx
-	push rdx
-	call rcx
-	pop rdx
-	pop rcx
-	pop rbx
-	pop rdi
+	mov rsi, r13
+	call r14
 	cmp rax, 0
 	je .remove_first
 .loop_remains:
@@ -39,17 +43,10 @@ ft_list_remove_if:
 	jz .finish
 	cmp qword [rbx], 0
 	je .finish
-	push rdi
 	mov rax, [rbx]
 	mov rdi, [rax + 8]
-	push rbx
-	push rcx
-	push rdx
-	call rcx
-	pop rdx
-	pop rcx
-	pop rbx
-	pop rdi
+	mov rsi, r13
+	call r14
 	cmp rax, 0
 	je .remove_in_list
 .add_values:
@@ -58,29 +55,27 @@ ft_list_remove_if:
 
 
 .remove_in_list:
-	push rcx
-	mov rax, [rbx]
-	mov rcx, [rax]
-	mov qword [rbx], rcx
-	push rdi
-	mov rcx, rax
-	mov rdi, [rcx + 8]
-	call rdx
-	pop rdi
-	pop rcx
+	mov r8, [rbx]
+	mov r9, [r8]
+	mov qword [rbx], r9
+	mov rdi, [r8 + 8]
+	call r15
+	mov rdi, r8
+	call free wrt ..plt
 	jmp .add_values
 .remove_first:
-	push rbx
-	mov rax, [rdi]
-	mov qword [rbx], rax
-	mov [rdi], rbx
-	pop rbx
-	push rdi
+	mov rax, [rbx]
+	mov [r12], rax
 	mov rdi, [rbx + 8]
-	call rdx
-	pop rdi
+	call r15
+	mov rdi, rbx
+	call free wrt ..plt
 	jmp .first_element
 .finish: 
+	pop r15
+	pop r14
+	pop r13
+	pop r12
 	ret
 
 section .note.GNU-stack
--- main.c
+++ main.c
@@ -177,17 +177,17 @@ int main(void)
 
 	for (int i = 0; i< 10; i++)
 	{
-		if (i == 0)
+		if (i <= 5)
 			sprintf(buff, "coucou%d", 1);
 		else
 			sprintf(buff, "coucou%d", 2);
 		ft_list_push_front(&lst, ft_strdup(buff));
 	}
-	tmp = lst;
 	printf("size : %d\n", ft_list_size(lst));
 
 	ft_list_sort(&lst, ft_strcmp);
-	ft_list_remove_if(&lst, "coucou1", ft_strcmp, free);
+	ft_list_remove_if(&lst, "coucou2", ft_strcmp, free);
+	tmp = lst;
 
 	while (tmp)
 	{
