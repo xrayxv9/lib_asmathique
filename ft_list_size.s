cecc980f6f8d904514cdde1abf45e2ffeebee44d
xray <morel.clement@proton.me>
2025-11-27 21:02:36 +0100

    everything is now finished

--- ft_list_size.s
+++ ft_list_size.s
@@ -3,21 +3,25 @@ section .text
 global ft_list_size
 
 ft_list_size:
-	test rdi, rdi
-	jz .error
-	mov rax, rdi
-	mov rbx, 0
+	push	rbp
+	mov		rbp, rsp
+	test	rdi, rdi
+	jz		.error
+	mov		rax, rdi
+	xor		r8, r8
 .loop:
-	test rax, rax
-	jz .finish
-	inc rbx
-	mov rax, [rax]
-	jmp .loop
+	test	rax, rax
+	jz		.finish
+	inc		r8
+	mov		rax, [rax]
+	jmp		.loop
 .finish:
-	mov rax, rbx
-	ret
+	mov		rax, r8
+	jmp		.ret
 .error:
-	mov rax, 0
+	mov		rax, 0
+.ret:
+	pop		rbp
 	ret
 
 section .not.GNU-stack
